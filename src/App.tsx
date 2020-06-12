5cbf2326fdc65700462fefe0f00d3d4fc5daf68a
Matheus Filipe <mfcr031@gmail.com>
2020-06-12 16:16:32 -0300

    ✨ start

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+
+import { StatusBar } from 'react-native';
+
+import Routes from './routes';
+
+const App: React.FC = () => (
+  <NavigationContainer>
+    <StatusBar barStyle="light-content" backgroundColor="#000" />
+    <Routes />
+  </NavigationContainer>
+);
+
+export default App;
