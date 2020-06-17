070a02d4a02ea38d51f53af5f2b4d5e6e55b79fe
Matheus Filipe <mfcr031@gmail.com>
2020-06-17 20:42:05 -0300

    🔧 finish create appointment route

--- src/modules/client/routes/client.routes.tsx
+++ src/modules/client/routes/client.routes.tsx
@@ -1,18 +1,63 @@
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import { Platform } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import Icon from 'react-native-vector-icons/Feather';
 
 import Dashboard from '../pages/Dashboard';
 
-const App = createStackNavigator();
+import NewAppointment from './newappointment.routes';
+
+interface ITabBarIcon {
+  focused: boolean;
+}
+
+const Tab = createBottomTabNavigator();
+
+const tabBarOptions = {
+  keyboardHidesTabBar: true,
+  activeTintColor: '#ff9000',
+  inactiveTintColor: '#fff',
+  style: {
+    backgroundColor: '#17181d',
+    marginTop: 1,
+    paddingTop: 5,
+    height: Platform.OS === 'ios' ? 100 : 65,
+  },
+  labelStyle: {
+    marginBottom: 10,
+  },
+};
+
+const dashboardOptions = {
+  tabBarLabel: 'Agendamentos',
+  tabBarIcon: ({ focused }: ITabBarIcon) => (
+    <Icon name="calendar" size={20} color={focused ? '#ff9000' : '#fff'} />
+  ),
+};
+
+const newAppointmentsOptions = {
+  // unmountOnBlur: true,
+  tabBarVisible: false,
+  tabBarLabel: 'Agendar',
+  tabBarIcon: ({ focused }: ITabBarIcon) => (
+    <Icon name="plus" size={20} color={focused ? '#ff9000' : '#fff'} />
+  ),
+};
 
 const ClientRoutes: React.FC = () => (
-  <App.Navigator
-    screenOptions={{
-      headerShown: false,
-    }}
-  >
-    <App.Screen name="Dashboard" component={Dashboard} />
-  </App.Navigator>
+  <Tab.Navigator tabBarOptions={tabBarOptions}>
+    <Tab.Screen
+      name="Dashboard"
+      component={Dashboard}
+      options={dashboardOptions}
+    />
+    <Tab.Screen
+      name="Agendar"
+      component={NewAppointment}
+      options={newAppointmentsOptions}
+    />
+  </Tab.Navigator>
 );
 
 export default ClientRoutes;
---
+++ src/modules/client/routes/newappointment.routes.tsx
@@ -0,0 +1,129 @@
+import React from 'react';
+import { TouchableOpacity, Platform } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { useNavigation } from '@react-navigation/native';
+
+import Icon from 'react-native-vector-icons/Feather';
+
+import SelectProvider from '../pages/NewAppointment/SelectProvider';
+import SelectDate from '../pages/NewAppointment/SelectDate';
+import SelectServiceAndConfirm from '../pages/NewAppointment/SelectServiceAndConfirm';
+
+const Stack = createStackNavigator();
+
+const stackNewOptions = {
+  headerTintColor: '#fff',
+  headerLeftContainerStyle: {
+    marginLeft: 30,
+  },
+  cardStyle: {
+    backgroundColor: 'black',
+  },
+  // animationEnabled: false,
+};
+
+const NewAppointment: React.FC = () => {
+  const navigation = useNavigation();
+
+  return (
+    <Stack.Navigator screenOptions={stackNewOptions} headerMode="screen">
+      <Stack.Screen
+        name="SelectProvider"
+        component={SelectProvider}
+        options={{
+          title: 'Barbeiros',
+          headerTitleAlign: 'center',
+          headerLeft: () => (
+            <TouchableOpacity
+              onPress={() => {
+                navigation.navigate('Dashboard');
+              }}
+            >
+              <Icon name="chevron-left" size={20} color="#fff" />
+            </TouchableOpacity>
+          ),
+          headerStyle: {
+            backgroundColor: '#18171d',
+            shadowOffset: {
+              height: 0,
+            },
+            height: Platform.OS === 'ios' ? 100 : 80,
+          },
+          headerTitleStyle: {
+            fontFamily: 'RobotoSlab-Regular',
+            fontSize: 20,
+          },
+          headerLeftContainerStyle: {
+            marginLeft: 30,
+          },
+        }}
+      />
+
+      <Stack.Screen
+        name="SelectDate"
+        component={SelectDate}
+        options={{
+          title: 'Data',
+          headerTitleAlign: 'center',
+          headerLeft: () => (
+            <TouchableOpacity
+              onPress={() => {
+                navigation.navigate('SelectProvider');
+              }}
+            >
+              <Icon name="chevron-left" size={20} color="#fff" />
+            </TouchableOpacity>
+          ),
+          headerStyle: {
+            backgroundColor: '#18171d',
+            shadowOffset: {
+              height: 0,
+            },
+            height: Platform.OS === 'ios' ? 100 : 80,
+          },
+          headerTitleStyle: {
+            fontFamily: 'RobotoSlab-Regular',
+            fontSize: 20,
+          },
+          headerLeftContainerStyle: {
+            marginLeft: 30,
+          },
+        }}
+      />
+
+      <Stack.Screen
+        name="SelectServiceAndConfirm"
+        component={SelectServiceAndConfirm}
+        options={{
+          title: 'Serviços',
+          headerTitleAlign: 'center',
+          headerLeft: () => (
+            <TouchableOpacity
+              onPress={() => {
+                navigation.navigate('SelectDate');
+              }}
+            >
+              <Icon name="chevron-left" size={20} color="#fff" />
+            </TouchableOpacity>
+          ),
+          headerStyle: {
+            backgroundColor: '#18171d',
+            shadowOffset: {
+              height: 0,
+            },
+            height: Platform.OS === 'ios' ? 100 : 80,
+          },
+          headerTitleStyle: {
+            fontFamily: 'RobotoSlab-Regular',
+            fontSize: 20,
+          },
+          headerLeftContainerStyle: {
+            marginLeft: 30,
+          },
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+export default NewAppointment;
