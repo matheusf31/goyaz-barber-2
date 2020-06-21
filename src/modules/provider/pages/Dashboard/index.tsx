939288579d98626fd373e689b2aff4aaa58f4f74
Matheus Filipe <mfcr031@gmail.com>
2020-06-21 13:18:05 -0300

    🔧 update

--- src/modules/provider/pages/Dashboard/index.tsx
+++ src/modules/provider/pages/Dashboard/index.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
-import { Text, Button } from 'react-native';
+
+// import api from '../../../../shared/services/api';
+
+import Background from '../../../../shared/components/Background';
 
 import { Container } from './styles';
 
-import { useAuth } from '../../../../shared/hooks/auth';
-
 const Dashboard: React.FC = () => {
-  const { signOut } = useAuth();
-
   return (
-    <Container>
-      <Text style={{ alignSelf: 'center', color: '#000' }}>Barbeiro</Text>
-
-      <Button title="Sair" onPress={signOut} />
-    </Container>
+    <Background>
+      <Container />
+    </Background>
   );
 };
 
