a92beee20d72307d4bb2b47043c864e343ed7cdf
Matheus Filipe <mfcr031@gmail.com>
2020-07-21 13:20:57 -0300

    .

--- src/shared/services/api.ts
+++ src/shared/services/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+// If you using the Android Studio emulator the baseURL is: 'https://10.0.2.3:3333'
+// If you using the IOS emulator the baseURL is: 'https://localhost:3333'
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: 'http://10.0.2.3:3333',
 });
 
 export default api;
