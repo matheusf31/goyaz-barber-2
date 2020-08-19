4dbb1fc66f9fc7ea4282260e9de618218d23fdb2
Matheus Filipe <mfcr031@gmail.com>
2020-08-19 11:12:36 -0300

    att ios

--- src/shared/services/socket.ts
+++ src/shared/services/socket.ts
@@ -1,5 +1,5 @@
 import io from 'socket.io-client';
 
-export const socket = io('http://192.168.0.4:3333');
+export const socket = io('http://192.168.0.9:3333');
 
 socket.on('connect', () => console.log('App conectado ao socket'));
