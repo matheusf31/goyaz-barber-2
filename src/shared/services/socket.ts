200e894df932937328e8646b53ad501c5b1cdc81
Matheus Filipe <mfcr031@gmail.com>
2020-08-26 23:00:33 -0300

    update

--- src/shared/services/socket.ts
+++ src/shared/services/socket.ts
@@ -2,4 +2,5 @@ import io from 'socket.io-client';
 
 export const socket = io('http://192.168.0.12:3333');
 
+// eslint-disable-next-line no-console
 socket.on('connect', () => console.log('App conectado ao socket'));
