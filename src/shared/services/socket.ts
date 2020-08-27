9ba73d567f6a41578a5a0e387504340474b73566
Matheus Filipe <mfcr031@gmail.com>
2020-08-26 22:59:08 -0300

    update style

--- src/shared/services/socket.ts
+++ src/shared/services/socket.ts
@@ -1,5 +1,5 @@
 import io from 'socket.io-client';
 
-export const socket = io('https://goyazbarber.tecteu.com');
+export const socket = io('http://192.168.0.12:3333');
 
 socket.on('connect', () => console.log('App conectado ao socket'));
